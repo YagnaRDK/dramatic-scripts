471d8341940184af0a91c7bb983241e7458f50d1
YagnaRDK <yagna.r.d.khokhariya@gmail.com>
2023-08-31 15:30:18 +0530

    soundpack script

---
+++ app/scripts/sound_pack/page.js
@@ -0,0 +1,88 @@
+import Image from "next/image";
+import Footer from "@/app/components/footer";
+import Header from "@/app/components/header";
+
+export default function SoundPack() {
+  return (
+    <>
+      <Header />
+      <div className="min-h-screen flex flex-col items-center place-content-center w-full space-y-5">
+        <iframe
+          width="1920"
+          height="1080"
+          src="https://www.youtube.com/embed/WEZggl_kQYs"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          className="w-auto h-auto mt-16 md:w-[800px] md:h-[450px] rounded-2xl"
+          allowfullscreen
+        ></iframe>
+        <hr className=" rounded-full w-8/12 self-center"></hr>
+
+        <p className="w-7/12 font-bold text-2xl underline decoration-indigo-500 decoration-4 text-slate-200 md:text-4xl">
+          Description:
+        </p>
+
+        {/* <br></br><br></br> */}
+        <p className="w-7/12 indent-6 font-bold text-sm text-slate-200 md:text-lg">
+          The Dramatic Soundpack represents a meticulously crafted, fully
+          customized sound experience tailored specifically for FiveM servers.
+          Compatible with a multitude of frameworks, this stand-alone script
+          elevates the gaming atmosphere by enhancing the sounds of guns,
+          explosions, and vehicles. It immerses players in a more interactive
+          and authentic roleplay environment, promising continuous updates and
+          the addition of new sounds in the future.
+        </p>
+
+        <p className="w-7/12 font-bold text-2xl underline decoration-indigo-500 decoration-4 text-slate-200 md:text-4xl">
+          Key Features:
+        </p>
+        <ul className="w-7/12 font-bold text-sm text-slate-200 md:text-lg list-outside list-disc">
+          <li>Custom sound added for multiple weapons.</li>
+          <li>All the sound&apos;s are server side.</li>
+          <li>Custom vehicles sounds for multiple vehicle.</li>
+          <li>Custom explosion sounds for multiple type of explosions.</li>
+        </ul>
+
+        <p className="w-7/12 font-bold text-2xl underline decoration-indigo-500 decoration-4 text-slate-200 md:text-4xl">
+          Additional Information:
+        </p>
+
+        <p className="w-7/12 indent-6 font-bold text-sm text-slate-200 md:text-lg">
+          This package is constantly evolving, with each image or feature in
+          this publication being subject to continuous improvement and
+          adaptation for the optimal experience of the end consumer.<br></br>
+          <br></br>Please be aware that this package is exclusively sold through
+          Tebex and is protected by FiveM Asset Escrow. We do not offer this
+          script for sale through any other platforms.<br></br>
+          <br></br>Refunds are not accepted once the purchase has been made. The
+          package is in exceptional and fully functional condition. In the event
+          that a chargeback is initiated against this resource, both Tebex and
+          Dramatic Store may take appropriate action due to the violation of
+          multiple terms outlined in Tebex&apos;s policies. For more information
+          regarding these policies,{" "}
+          <a href="https://checkout.tebex.io/terms" className="text-indigo-500">
+            click here
+          </a>
+          .
+        </p>
+        <div className="w-7/12 flex flex-row justify-end text-white">
+          <a
+            // className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg"
+            className="btn btn-md lg:btn-lg text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            href="https://dramatic.tebex.io/package/5879140"
+          >
+            {/* <Image
+              src="/shop.png"
+              alt="support vector drawing"
+              height={6}
+              width={40}
+            ></Image> */}
+            Buy Now
+          </a>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
