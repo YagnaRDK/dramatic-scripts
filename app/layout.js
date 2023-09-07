4c38477ec2dfb00495f3759f8caa73ea220279db
YagnaRDK <yagna.r.d.khokhariya@gmail.com>
2023-09-07 11:13:53 +0530

    meta data

--- app/layout.js
+++ app/layout.js
@@ -12,29 +12,54 @@ export const metadata = {
   title: "Dramatic Scripts",
   description:
     "Empower your GTA 5 roleplay dreams with affordable & custom scripts.",
-  // keywords: [
-  //   "Dramatic",
-  //   "roleplay",
-  //   "scripts",
-  //   "GTA",
-  //   "5",
-  //   "affordable",
-  //   "custom",
-  //   "Next.js",
-  //   "React",
-  //   "JavaScript",
-  // ],
+  keywords: [
+    "Dramatic",
+    "Dramatic Scripts",
+    "Roleplay",
+    "Scripts",
+    "GTA",
+    "GTA5",
+    "FiveM",
+    "Affordable",
+    "Custom",
+    "Next.js",
+    "React",
+    "JavaScript",
+  ],
   // authors: [
   //   { name: "Aayush Shankar" },
   //   { name: "Yagna R.D. Khokhariya", url: "https://twitter.com/YagnaRDK" },
   // ],
-  // colorScheme: "dark",
+  colorScheme: "dark",
+  // Image: "/drs-thumbnail.png",
   // creator: "Yagna R.D. Khokhariya",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        name="keywords"
+        content="Dramatic, Dramatic Scripts, DRS, GTA5, Roleplay, Scripts, FiveM"
+      ></meta>
+      <meta property="og:title" content="Dramatic Scripts" />
+      <meta
+        property="og:description"
+        content="Empower your GTA 5 roleplay dreams with affordable & custom scripts."
+      />
+      <meta property="og:image" content="/drs-thumbnail.png" />
+
+      {/* For Twitter */}
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://dramaticrp.in/" />
+      <meta property="twitter:title" content="Dramatic Scripts" />
+      <meta
+        property="twitter:description"
+        content="Empower your GTA 5 roleplay dreams with affordable & custom scripts."
+      />
+      <meta property="twitter:image" content="/drs-thumbnail.png" />
       <body>{children}</body>
       {/* className={clsx(manrope.className, "bg-black")} */}
     </html>
