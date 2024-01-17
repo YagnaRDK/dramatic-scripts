ed7ce7b2d687bb46144c14a3df78ee7b3e4ac042
YagnaRDK <yagna.r.d.khokhariya@gmail.com>
2024-01-17 15:29:06 +0530

    instagram link change

--- app/instagram/page.js
+++ app/instagram/page.js
@@ -2,7 +2,7 @@ import InstagramLayout from "./layout";
 
 import { redirect } from "next/navigation";
 export default async function Instagram() {
-  redirect("https://www.instagram.com/dramaticscripts/");
+  redirect("https://instagram.com/dramaticscripts/");
 }
 
 Instagram.getLayout = function getLayout(page) {
