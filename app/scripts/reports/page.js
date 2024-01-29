3fdca6a57d2f35daca8faa1c12ef9d85618ee882
YagnaRDK <yagna.r.d.khokhariya@gmail.com>
2024-01-29 14:57:43 +0530

    Reports YouTube link update

--- app/scripts/reports/page.js
+++ app/scripts/reports/page.js
@@ -10,7 +10,7 @@ export default function Yikes() {
         <iframe
           width="1920"
           height="1080"
-          src="https://www.youtube.com/embed/gamemaster9"
+          src="https://www.youtube.com/embed/dvCMucUA5gU"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
