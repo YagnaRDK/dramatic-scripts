03f3ac2b42bf68353ccd30e51066163e2a63f75c
YagnaRDK <yagna.r.d.khokhariya@gmail.com>
2024-02-05 09:50:46 +0530

    /pizza-delivery -> /pizzadelivery

--- app/scripts/page.js
+++ app/scripts/page.js
@@ -142,7 +142,7 @@ export default function ScriptsPage() {
                     role="button"
                     // className="btn glass btn-primary rounded-lg"
                     className="btn text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-                    href="/scripts/pizza-delivery"
+                    href="/scripts/pizzadelivery"
                   >
                     Know more{" "}
                     <svg
